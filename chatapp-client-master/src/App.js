b54cbc4e1de1a0002aaa0134e8b5395377df5b45
yun <yshyeon102@gmail.com>
2023-12-18 14:49:09 +0900

    feat: front code 추가
    https://www.youtube.com/watch?v=pRGOEtGjI-k&t=306s

--- chatapp-client-master/src/App.js
+++ chatapp-client-master/src/App.js
@@ -1,11 +1,48 @@
+import {useEffect, useState} from "react";
 import "./App.css";
+import socket from "./server";
+import InputField from "./components/InputField/InputField";
+import MessageContainer from "./components/MessageContainer/MessageContainer";
 
 function App() {
-  return (
-    <div>
-      <div className="App"></div>
-    </div>
-  );
+    const [user, setUser] = useState(null);
+    const [message, setMessage] = useState('');
+    const [messageList, setMessageList] = useState([]);
+
+    console.log("messageList : ", messageList);
+
+    useEffect(() => {
+        socket.on("message", (res) => {
+            console.log("res", message);
+            setMessageList((prevState) => prevState.concat(message));
+        })
+        askUserName();
+    }, []);
+
+    const askUserName = () => {
+        const userName = prompt("당신의 이름을 입력하세요")
+        console.log("userName : ", userName);
+
+        socket.emit("login", userName, (res) => {
+            // console.log("Res", res)
+            if (res?.ok) {
+                setUser(res.data);
+            }
+        })
+    };
+
+    const sendMessage = (event) => {
+        event.preventDefault()
+        socket.emit("sendMessage", message, (res => {
+            console.log("sendMessage res", res)
+        }))
+    };
+    return (<div>
+            <div className="App">
+                <MessageContainer messageList={messageList} user={user}/>
+                <InputField message={message} setMessage={setMessage} sendMessage={sendMessage}/>
+            </div>
+        </div>);
 }
 
 export default App;
