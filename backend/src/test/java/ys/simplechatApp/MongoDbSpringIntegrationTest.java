184d0bc3a14373b9a6a878925c1a5dd78528f34a
vivalavida <vivalavida.devv@gmail.com>
2023-12-23 04:36:45 +0900

    test: Transaction 동작 확인

--- backend/src/test/java/ys/simplechatApp/MongoDbSpringIntegrationTest.java
+++ backend/src/test/java/ys/simplechatApp/MongoDbSpringIntegrationTest.java
@@ -7,18 +7,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ys.simplechatApp.config.MongoTransactionConfiguration;
 
 @DataMongoTest
+@Import(MongoTransactionConfiguration.class)
 @ExtendWith(SpringExtension.class)
+@Transactional
 public class MongoDbSpringIntegrationTest {
-    @DisplayName("given object to save"
-            + " when save object using MongoDB template"
-            + " then object is saved")
     @Test
     public void test(@Autowired MongoTemplate mongoTemplate) {
         // given
